a5dcf78734857410a5a06a0b438e4455693e2afc
Albert <reg16vp@mail.ru>
2023-06-23 13:08:51 +0300

    дописал скрипт по извлечению перфорированной мощности по датам. Заглушка на медиану. Нужно будет пересмотреть для точности

--- utils/get_perf_thick.py
+++ utils/get_perf_thick.py
@@ -3,21 +3,22 @@
 # @Time    : 2023/6/23
 # @Author  : <VAR>
 # @File    : get_perf_thick.py
-# @Description:
+# @Description: специально для Азамата
+# Для работы скрипта выгружаем из модели wellpick файлы с параметром по x:y:z и i:j:k
+# WARN: для корректной работы нужно выгрузить оба файла со сходими настройками
+# Объект:траектория Экспорт значения:значение вдоль скважины
+# и положить в папку reports в основной папке проекта
+# файлы называет "MD.inc" и "ijk.inc"
 
 
 import pandas as pd
 import os
+import numpy as np
 
-date = '01.01.2013'
-parametr = 'permx'
-m = get_model_by_name('BLACK_OIL_DEMO')
-# Для работы скрипта выгружаем из модели wellpick файлы с параметром по x:y:z и i:j:k
-# данные по требуемому параметру с координатами x y
+m = get_model_by_name('SARAI')
+
 dept_file = 'MD.inc'
-# данные по требуемому параметры с координатами i j k
 ijk_file = 'ijk.inc'
-# данные по зонам вскрытия пластов с координатами i j k для разбивки по пластам
 
 
 def create_report_dir(path):
@@ -36,42 +37,39 @@ print(os.getcwd())
 
 # Загрузка информации из файлов
 depth = pd.read_table(dept_file, index_col=None, skiprows=4, sep=' ', names=[
-                      'well', 'x', 'y', 'depth', parametr], usecols=[0, 1, 2, 3, 5])
-ijk = pd.read_table(ijk_file, index_col=None, skiprows=3, sep=' ', names=[
-                    'well', 'i', 'j', 'k', 'del', parametr], usecols=[0, 1, 2, 3, 5])
+                      'well', 'depth'], usecols=[0, 3])
+ijk = pd.read_table(ijk_file, index_col=None, skiprows=4, sep=' ', names=[
+                    'well', 'i', 'j', 'k'], usecols=[0, 1, 2, 3])
 
 
 # формирование информации из модели
-
-t = [x for x in get_all_timesteps() if x.name == date]
-t = t[0]
 
 model_frame = []
 for t in get_all_timesteps():
     for w in get_all_wells():
         for c in w.connections:
-            model_frame.append([str(c.well), t.name, c.i, c.j, c.k, float(clpr[m, c, t]), float(cwir[m, c, t]))])
+            if bool(c.is_opened()[t]):
+                # print([str(c.well), t.name,bool(c.is_opened( )[t]), c.i, c.j, c.k, float(clpr[m, c, t]), float(cwir[m, c, t])])
+                model_frame.append([str(c.well), t.name, c.i, c.j, c.k, float(
+                    clpr[m, c, t]), float(cwir[m, c, t])])
 
-model_frame=pd.DataFrame(model_frame)
-
-model_frame.columns=['well', 'i', 'j', 'k', 'lpr', 'lpt', 'wwir', 'wwit']
-model_frame.well=model_frame.well.astype('str')
-
-# предобработка данных
-merged_picks=pd.merge(left = depth, right = ijk, left_on = [
-                        'well', parametr], right_on = ['well', parametr])
-merged_picks['well']=merged_picks['well'].str.replace("'", "")
-
-
-# merged_picks=pd.merge(left = merged_picks, right = model_frame, how = 'left',
-#                         left_on = ['well', 'i', 'j', 'k'], right_on = ['well', 'i', 'j', 'k'])
+model_frame = pd.DataFrame(model_frame)
+model_frame.columns = ['well', 'date', 'i', 'j', 'k', 'lpr', 'wwir']
+model_frame.well = model_frame.well.astype('str')
 #
-# merged_picks.fillna(0, inplace = True)
+# # предобработка данных
+df_thick = pd.concat([ijk, depth.depth], axis=1)
+df_thick['well'] = df_thick['well'].str.replace("'", "")
+df_thick['thick'] = df_thick.depth.diff()
+df_thick['thick'] = df_thick['thick'].median()
+df_thick = pd.merge(left=model_frame, right=df_thick, how='left',
+                    left_on=['well', 'i', 'j', 'k'], right_on=['well', 'i', 'j', 'k'])
 #
-# merged_picks=merged_picks.groupby(['well', 'zone']).agg({'i': 'min', 'j': 'min', 'k': ['min', 'max'], 'lpr': 'sum',
-#                                                            'lpt': 'sum', 'wwir': 'sum', 'wwit': 'sum',
-#                                                            'depth': ['min', 'max'], parametr: 'median'})  # .reset_index()
-# merged_picks=merged_picks.reset_index()
-# merged_picks.columns=['well', 'zone', 'i', 'j', 'k', 'k2', 'lpr',
-#                         'lpt', 'wwir', 'wwit', 'depth', 'depth2', parametr]
-# merged_picks.to_excel('wells_profile.xlsx')
+# Агрегируем финальный датафрейм
+# merged_picks = merged_picks.groupby(['well', 'date']).agg({'k': ['min', 'max'], 'lpr': 'sum',
+# 'wwir': 'sum', 'depth': ['min', 'max'], 'thick': 'sum'})  # .reset_index()
+df_thick = df_thick.groupby(['well', 'date']).agg(
+    {'thick': 'sum'})
+df_thick = df_thick.reset_index()
+
+df_thick.to_csv('perf_thickness.csv')
