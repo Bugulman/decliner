f4313cf6664c2735fee5913266df0c28d61fd8a0
Альберт Вафин <reg16vp@mail.ru>
2023-07-03 09:45:52 +0500

    Обработал RFT для записи в базу.

--- parsers/RFT_las_parse.py
+++ parsers/RFT_las_parse.py
@@ -1,5 +1,4 @@
 import lasio as ls
-import os
 import py7zr
 import zipfile
 import rarfile
@@ -9,7 +8,6 @@ from pathlib import Path
 import re
 import pandas as pd
 from sqlalchemy import create_engine
-from bs4 import BeautifulSoup
 import pprint
 import sqlite3
 from datetime import datetime, time
@@ -95,7 +93,7 @@ def get_date(string_with_date, regex=r'\d{1,2}\.\d{2}\.\d{2,4}'):
     return date
 
 
-def get_curves(las_file, regex=r'.*manom[a-z]*|.*debit[a-z]*|.*rashod[a-z]*'):
+def get_curves(las_file, regex=r'.*[Mm]anom[a-z]*|.*[Dd]ebit[a-z]*|.*[Rr]ashod[a-z]*|.*[Ss]hum[a-z]*|.*[Tt]ermo'):
     """docstring for get_curves"""
     aim = re.compile(regex)
     tar_curves = [curve.descr.lower() for curve in las_file.curves
@@ -104,10 +102,10 @@ def get_curves(las_file, regex=r'.*manom[a-z]*|.*debit[a-z]*|.*rashod[a-z]*'):
 
 
 def rft_las_parse(file):
-    """Функция переводит RFT исследования их формата las в df"""
+    """Функция переводит RFT исследования из формата las в df"""
     las = ls.read(file)
     df = las.df()
-    curves = get_curves(las, regex=r'.*manom[a-z]*')
+    curves = get_curves(las)
     # print(las.curves)
     mnem_names = [
         curve.mnemonic for curve in las.curves if curve.descr.lower() in curves]
@@ -120,6 +118,7 @@ def rft_las_parse(file):
         dif_name = cols+'_diff'
         df[dif_name] = df[cols].diff()
     wellname, logdate = get_metadata(las)
+    df = df.stack().reset_index()
     df.insert(0, 'Well', wellname)
     df.insert(1, 'Date', logdate)
     return df.reset_index()
@@ -132,11 +131,26 @@ with open('RFT_erorrs.txt', 'w+') as var:
         try:
             df = rft_las_parse(file)
             df['Path'] = str(file)
-            las = ls.read(file)
-            well, date = get_metadata(las)
-            name = f'Well_{well}_{date}.csv'
-            df.columns = ['dept', 'well', 'date',
-                          'manom', 'diff_manom', 'path']
+            # las = ls.read(file)
+            # well, date = get_metadata(las)
+            # name = f'Well_{well}_{date}.csv'
+            # df.columns = ['dept', 'well', 'date',
+            # 'manom', 'diff_manom', 'path']
+            # df.to_csv(root_dir.joinpath('RFT', name))
+            df.to_sql('rft', con=engine, if_exists='append')
+        except:
+            var.write(str(file)+'\n')
+            print(file)
+            continue
+    for file in root_dir.glob('**/*.las*'):
+        try:
+            df = rft_las_parse(file)
+            df['Path'] = str(file)
+            # las = ls.read(file)
+            # well, date = get_metadata(las)
+            # name = f'Well_{well}_{date}.csv'
+            # df.columns = ['dept', 'well', 'date',
+            # 'manom', 'diff_manom', 'path']
             # df.to_csv(root_dir.joinpath('RFT', name))
             df.to_sql('rft', con=engine, if_exists='append')
         except:
@@ -144,24 +158,29 @@ with open('RFT_erorrs.txt', 'w+') as var:
             print(file)
             continue
 
+
 # %% BUG: отладки и мусор
 las = Path(
     r'/cluster3/public/2023/UNGKM/Data Achimgas/ПГИ/9 куст/А 9-5/21.06.2018/las/A9-5_Ureng_pgi_21-23.06.2018_statika.las')
 
 las = Path(
-    r'/cluster3/public/2023/UNGKM/Data Achimgas/ПГИ/4 куст/А 4-6/23.06.2017/А4-6_Уренгойское_ПГИ_2017_/4-6_Ureng_rgd_24.06.2017.las'
+    r'/cluster3/public/2023/UNGKM/Data Achimgas/ПГИ/unzipped_files/15.09.2013/LAS/A4-3_STAT.LAS'
 )
 
 las_file = ls.read(las)
 las_file.sections.keys()  # ['Other information']
+get_curves(las_file)
 las_file.sections['Other']  # ['Other information']
 
 fr = rft_las_parse(las)
-fr
+fr.level_1.unique()
+fr.columns
+fr.DEPT = fr.DEPT.astype('str')
+fr.info()
+fr.to_sql('rft', con=engine, if_exists='append')
+fr.unstack().reset_index()
 well, date = get_metadata(las_file)
 name = f'Well_{well}_{date}.csv'
 fr.to_sql('rft', con=engine, if_exists='append')
 
 df.to_csv(root_dir.joinpath('RFT', name))
-
-fr.columns
