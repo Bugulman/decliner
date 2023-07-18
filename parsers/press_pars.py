2f7a89d5911b264c31a660e741819f6726490e4b
Альберт Вафин <reg16vp@mail.ru>
2023-07-18 12:49:30 +0500

    вывел все общие функции в отдельный файл

--- parsers/press_pars.py
+++ parsers/press_pars.py
@@ -1,26 +1,12 @@
 # %%
-import os
-import time
+from support_func import file_runer, convert_excel_KVD, convert_csv_KVD
 from pathlib import Path
-import re
-import pandas as pd
+from tqdm import tqdm
 from sqlalchemy import create_engine
-import pprint
-import sqlite3
-import csv
-from datetime import datetime, time
 import warnings
+import sys
+sys.path.append('/home/albert.vafin/Documents/python_proj/decliner/parsers')
 warnings.filterwarnings('ignore')
-p = Path.cwd()
-p
-# con = sqlite3.connect(p.joinpath('pasport.db'))
-
-# con.close()
-# %%
-# NOTE:подключение к базе Postgresql
-engine = create_engine(
-    'postgresql://test:test@localhost:5434/ungkm')
-# %%
 
 # NOTE: подключение к базе sqllite
 engine = create_engine('sqlite:///press.db')
@@ -30,138 +16,32 @@ engine = create_engine('sqlite:///press.db')
 # Перечень файлов паспортов скважин
 # WARN: запускается единожды!
 p = Path(r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений')
-print(p)
 
-with open('press.txt', 'w+') as var:
-    for file in p.glob('**/*.CSV*'):
-        finder = re.compile('GA.*')
-        if finder.search(str(file)) != None:
-            try:
-                var.write(str(file)+'\n')
-                print(file)
-            except:
-                continue
-# %%
-
-text = r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений/2014/забойники от 29.07.14/18 куст/JUL14/GA140701.CSV'
-
-text = r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений/2022/КВД скв. №А4-3/GA220818.CSV'
-# %%
-
-# FAQ:обработка даты
-p = Path(text)
-str(p)
-
-
-def get_date_from_filename(path):
-    """функция принимает путь к файлу и формирует дату из названия файла
-    в формате yymmdd"""
-    date = re.findall('\d{6}', path.parts[-1])
-    date = datetime.strptime(date[0], '%y%m%d')
-    return date
 
 # %%
-# FAQ:обработка единичного csv файла
-
-
-def get_wells(file_path: str):
-    """функция читает заголовок csv файла и заирает оттуда информация по 
-    имеющимся в файле скважинам"""
-    header = []
-    with open(file_path, 'r', encoding='cp1251') as var:
-        line = csv.reader(var)
-        for row in line:
-            if len(row) <= 2:
-                header.append(row[0].strip())
-            else:
-                break
-    return header
-
-
-def main(file_path: Path):
-    header = get_wells(str(file_path))
-    date = get_date_from_filename(file_path)
-    full_frame = pd.read_csv(file_path, skiprows=len(
-        header)+2, engine='python', encoding='cp1251')
-    # print(header)
-    for seq in range(len(header)):
-        print(header[seq])
-        subframe = full_frame.iloc[:, [0, 2*seq+1, 2*seq+2]]
-        # print(subframe.head())
-        subframe.columns = ['time', 'press', 'temp']
-        # print(subframe.head())
-        subframe.time = pd.to_timedelta(subframe.time, errors='coerce')
-        subframe = subframe[~subframe.time.isnull()]
-        subframe.time = subframe.time+date
-        # print(subframe)
-        subframe.index = subframe.time
-        subframe.press = pd.to_numeric(subframe.press)
-        subframe.temp = pd.to_numeric(subframe.temp)
-        subframe.to_csv('testt.csv')
-        subframe = subframe[['press', 'temp']].resample('H').mean()
-        subframe['well'] = header[seq]
-        subframe['path'] = str(file_path)
-        subframe.to_sql('press', con=engine, if_exists='append')
-
-        # print(subframe)
-        # else:
-        # print(row[0], row[1], row[2])
-    # subframe.info()
+csv_files = file_runer(p, 'CSV', regex_file='GA.*') + \
+    file_runer(p, 'csv', regex_file='GA.*')
+excel_files = file_runer(p, 'xls') + \
+    file_runer(p, 'XLS')
 # %%
-text = r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений/2014/забойники от 29.07.14/20 куст/JUL14/GA140720.CSV'
-
-text = r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений/2014/забойники от 29.07.14/20 куст/JUN14/GA140602.CSV'
-p = Path(text)
-main(p)
-
-header = get_wells(str(p))
-date = get_date_from_filename(p)
-full_frame = pd.read_csv(p, skiprows=len(
-    header)+2, engine='python', encoding='cp1251')
-
-full_frame
-
-full_frame = full_frame.iloc[:, [0, 1, 2]]
-
-full_frame.columns = ['time', 'press', 'temp']
-
-full_frame.time = pd.to_timedelta(full_frame.time, errors='coerce')
-full_frame.iloc[115]
-full_frame = full_frame[~full_frame.time.isnull()]
-full_frame.press = pd.to_numeric(full_frame.press)
-full_frame.temp = pd.to_numeric(full_frame.temp)
-full_frame.index = full_frame.time
-full_frame[['press', 'temp']].resample('H').mean()
-
-full_frame[~full_frame.time.isnull()].to_csv('test.csv')
-main(p)
-# temp.index = temp.time
-# temp[['press', 'temp']].resample('H').mean()
-# temp.info()
-
+convert_excel_KVD(excel_files[1])
 # %%
-# WARN: запускается единожды!
-p = Path(r'/cluster3/public/2023/UNGKM/Data Achimgas/Замеры забойных давлений')
-# print(p)
+# NOTE: ИМПОРТ исхдников из экселей в базу данных
+with open('zaboi_excel.txt', 'w') as f:
+    for n in tqdm(excel_files):
+        try:
+            df = convert_excel_KVD(n)
+            df.to_sql('zaboi_excel', con=engine, if_exists='append')
+        except:
+            f.write(f'{n}\n')
+            print(n)
 
-with open('press.txt', 'w+') as var:
-    for file in p.glob('**/*.CSV*'):
-        finder = re.compile('GA.*')
-        if finder.search(str(file)) != None:
-            try:
-                main(file)
-                # print(file)
-            except:
-                print(f'NOT LOADED {file}')
-                var.write(str(file)+'\n')
-                continue
 
-# %%
-# FAQ:отработка регулярок
-kust_re = re.compile('/.{0,8}[Кк]уст.{0,8}/')
-well_re = re.compile('/.{0,8}[Сс]кв.{0,12}/')
-pprint.pprint(finder.findall(text))
-
-text
-pd.read_csv(text, skiprows=3, engine='python',
-            encoding='cp1251', names=['time', 'press', 'temp'])
+# %% NOTE: ИМПОРТ исхдников из csv в базу данных
+for n in tqdm(csv_files):
+    try:
+        df = convert_csv_KVD(n)
+        df.to_sql('zaboi_csv', con=engine, if_exists='append')
+    except:
+        print(f'not{n}')
+    # df.to_sql('zaboi_csv', con=engine, if_exists='append')
--- parsers/support_func.py
+++ parsers/support_func.py
@@ -1,44 +1,28 @@
-# %%
+import re
 from pathlib import Path
 import re
 import pandas as pd
 from sqlalchemy import create_engine
-from tqdm import tqdm
 import openpyxl
 import csv
 from datetime import datetime
-import warnings
-import matplotlib.pyplot as plt
-warnings.filterwarnings('ignore')
-p = Path.cwd()
-p
-# NOTE: подключение к базе sqllite
-engine = create_engine('sqlite:///press.db')
-
-# %%
-
-# WARN: запускается единожды!
-p = Path(r'/cluster3/public/2023/UNGKM/Data Achimgas/ГДИ, ГКИ')
-print(p)
+from pathlib import Path
 
 
 def file_runer(path: Path, file_ext: str, regex_file='.*'):
     '''Функция вытаскивает файлы c расширением
     file_ext из подпапок по пути path. Возможно использование
     регулярок для фильтрации названий файлов'''
-    with open('press.txt', 'w+') as var:
-        files = []
-        counter = 0
-        for file in path.glob(f'**/*.{file_ext}*'):
-            finder = re.compile(regex_file)
-            if finder.search(str(file)) != None:
-                try:
-                    var.write(str(file)+'\n')
-                    # print(file)
-                    counter += 1
-                    files.append(file)
-                except:
-                    continue
+    files = []
+    counter = 0
+    for file in path.glob(f'**/*.{file_ext}*'):
+        finder = re.compile(regex_file)
+        if finder.search(str(file)) != None:
+            try:
+                counter += 1
+                files.append(file)
+            except:
+                continue
     print(counter)
     return files
 
@@ -52,7 +36,7 @@ def get_noblank_rows(sheet):
 
 
 def df_resample(df):
-    """Функция преобразует датафрейм в почасовой формат 
+    """Функция преобразует датафрейм в почасовой формат
     с отбраковкой замеров ниже 3 квартиля"""
     resampled = df[df.press > df.press.quantile(0.05)
                    ].press.resample('1H').mean()
@@ -68,7 +52,7 @@ def get_date_from_filename(path):
 
 
 def get_wells(file_path: str):
-    """функция читает заголовок csv файла и заирает оттуда информация по 
+    """функция читает заголовок csv файла и заирает оттуда информация по
     имеющимся в файле скважинам"""
     header = []
     with open(file_path, 'r', encoding='cp1251') as var:
@@ -81,13 +65,18 @@ def get_wells(file_path: str):
     return header
 
 
+def convert_datetime_to_date(date):
+    return date.strftime('%d.%m.%Y') if type(date) == datetime else date
+
+
 def convert_excel_KVD(excel_file: Path):
     wb = openpyxl.load_workbook(excel_file)
     sheet = wb.get_sheet_by_name(wb.sheetnames[0])
     last_row = get_noblank_rows(sheet)
     name = sheet.cell(1, 1).value
     date = sheet.cell(2, 2).value
-    date = datetime.strptime(date, '%d.%m.%Y')
+    date = convert_datetime_to_date(date)
+    date = datetime.strptime(str(date), '%d.%m.%Y')
     df = pd.read_excel(excel_file, skiprows=3,
                        nrows=last_row-3, usecols=(0, 1),
                        names=["date", "press"])
@@ -109,7 +98,7 @@ def convert_csv_KVD(file_path: Path):
     # print(header)
     df = pd.DataFrame()
     for seq in range(len(header)):
-        print(header[seq])
+        # print(header[seq])
         subframe = full_frame.iloc[:, [0, 2*seq+1, 2*seq+2]]
         # print(subframe.head())
         subframe.columns = ['date', 'press', 'temp']
@@ -130,72 +119,3 @@ def convert_csv_KVD(file_path: Path):
         # subframe.to_sql('press', con=engine, if_exists='append')
         df = pd.concat([df, subframe], ignore_index=True)
     return df
-
-    # print(subframe)
-    # else:
-    # print(row[0], row[1], row[2])
-    # subframe.info()
-
-
-# %%
-# NOTE: АНАЛИЗ и создание списков файлов с различными расширениями
-# csv_files = file_runer('CSV', regex_file='GA.*')
-# excel_files = file_runer('xls')
-# txt_files = file_runer('txt')
-#
-# pprint(csv_files[0:10])
-# %%
-
-# NOTE: ИМПОРТ исхдников из экселей в базу данных
-for n in tqdm(excel_files):
-    try:
-        df = convert_excel_KVD(n)
-        df.to_sql('KVD_excel', con=engine, if_exists='append')
-    except:
-        print(n)
-
-
-# %%
-# NOTE: ИМПОРТ исхдников из csv в базу данных
-for n in tqdm(csv_files):
-    try:
-        df = convert_csv_KVD(n)
-        df.to_sql('KVD_csv', con=engine, if_exists='append')
-    except:
-        print(n)
-
-
-#  %%BUG:УПРАЖНЕНИЯ чтобы все работало как надо
-df = convert_excel_KVD(excel_files[33])
-df = convert_csv_KVD(csv_files[0])
-df.columns
-df.date.dt.day
-df.set_index(df.date, inplace=True)  # .date.dt.date
-df.press.groupby(pd.Grouper(freq='1H')).mean()
-
-
-# %%
-df.info()
-df.set_index(df.date, inplace=True)
-resampled = df[df.press > df.press.quantile(0.05)
-               ].press.resample('1H').mean()
-resampled.plot()
-plt.show()
-df[df.press == 0]
-# %%
-
-fr = pd.read_sql('KVD_excel', con=engine)
-fr['day'] = fr.kvd_time.dt.date
-fr.set_index(fr.kvd_time, inplace=True)  # .date.dt.date
-fr[['well', 'press']].groupby(pd.Grouper(key='well', freq='1D')).mean()
-fr.kvd_time.value_counts()
-# %% BUG: заплаточка
-for w in fr.well.unique():
-    sub = fr.loc[fr.well == w]
-    resampled_df = df_resample(sub).reset_index()
-    resampled_df['well'] = w.split(' ')[-1].replace('-', '')
-    print(resampled_df)
-    resampled_df.to_sql('resampled_KVD', con=engine, if_exists='append')
-# %%
-plt.show()
-sub.press.resample('1H').mean()
