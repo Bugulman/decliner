8ce2e9e425b670c2c0e2fa4e0870cac4400667ae
VAR_aorus <reg16vp@mail.ru>
2025-08-26 18:03:12 +0300

    актуальная жизнеспособная версия построения характеристики вытеснения. Пока не знаю насколько удобно в бою

--- WOR_forecast/wor_forecast.py
+++ WOR_forecast/wor_forecast.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from math import log
+from math import exp, log
 from scipy.optimize import curve_fit
 
 
@@ -27,10 +27,11 @@ class WOR_forecaster():
         self.wcth = df['wct'].to_numpy()
         self.R = Np/self.N
 
-    def sw_calc(self):
+    def sw_calc(self, OR):
         """Расчитываем среднее насыщение"""
-        self.Swav = self.R*(1-self.Swi)+self.Swi
-        self.Sw = self.lam*self.Swav+(1-self.lam)*(1-self.Sor)
+        Swav = OR*(1-self.Swi)+self.Swi
+        Sw = self.lam*Swav+(1-self.lam)*(1-self.Sor)
+        return Swav, Sw
 
     def lin_coef_calc(self, lam, R):
         """На основе из Бакл-Лев уравнения выводим коэффициенты для линизации уравнения"""
@@ -47,7 +48,7 @@ class WOR_forecaster():
         
         return x1, x2
 
-    def watercut_calc(self, R, lam, nw, no):
+    def watercut_from_wor(self, R, lam, nw, no):
         """Считаем обводненность от КИН"""
         x1, x2 = self.lin_coef_calc(lam, R)
         wor = np.exp(self.a + nw * x1 - no * x2)
@@ -56,14 +57,44 @@ class WOR_forecaster():
 
     def get_watercut(self):
         """Считаем обводненность от КИН"""
-        frame = np.column_stack((self.R, self.Swav, self.Sw, self.fw))
-        frame = pd.DataFrame(frame, columns=['R','Swav', 'Sw', 'fw'])
+        self.wct_fitting()
+        fw_model = self.watercut_from_wor(self.R, self.lam_fit, self.nw_fit, self.no_fit)
+        Swav, Sw = self.sw_calc(self.R)
+        frame = np.column_stack((self.R, Swav, Sw, fw_model, self.wcth))
+        frame = pd.DataFrame(frame, columns=['R','Swav', 'Sw', 'fw_model','fw_hist'])
+        return frame
+
+    def limit_calc(self,nw, no, fw):
+        exp_coeff = (3.5 * nw + 6.5 * no) / (nw + no)
+        power_coeff = (1.3 * nw + 0.7 * no) / (nw * (nw + no))
+        term1 = 1+0.006738*np.exp(exp_coeff)
+        wor_ratio = (1/self.M) * (fw/(1 - fw))
+        term2 = term1 * np.power(wor_ratio, power_coeff)
+        d = term2**(nw/no)
+        self.R_lim = 1/self.c*(1-1/d)
+        return self.R_lim
+
+    def wct_forecast(self):
+        self.wct_fitting()
+        R_lim = self.limit_calc(self.nw_fit, self.no_fit, 0.98)
+        RF_predict = np.linspace(self.R.max(), R_lim+0.1)
+        fw_predict = self.watercut_from_wor(RF_predict, self.lam_fit, self.nw_fit, self.no_fit)
+        Swav_predict, Sw_predic = self.sw_calc(RF_predict)
+        hist_df = self.get_watercut()
+        hist_df['Comment'] = 'history_data'
+        frame = np.column_stack((RF_predict, Swav_predict, Sw_predic, fw_predict))
+        frame = pd.DataFrame(frame, columns=['R','Swav', 'Sw', 'fw_model'])
+        frame['fw_hist'] = np.NAN
+        frame['Comment'] = 'forecast'
+        frame = pd.concat([hist_df, frame])
+        frame = frame.query('fw_model<0.98')
+        frame['OPT'] = self.N * frame['R']
         return frame
 
     def wct_fitting(self):
         """Подгонка параметров модели к историческим данным"""
         # Фильтруем данные, где R > 0 и wcth в допустимом диапазоне
-        valid_mask = (self.R > 0) & (self.wcth >= 0) & (self.wcth <= 1)
+        valid_mask = (self.R > 0) & (self.wcth >= 0) & (self.wcth <= 0.8)
         R_valid = self.R[valid_mask]
         wcth_valid = self.wcth[valid_mask]
         
@@ -80,7 +111,7 @@ class WOR_forecaster():
         try:
             # Выполняем подгонку
             popt, pcov = curve_fit(
-                self.watercut_calc, 
+                self.watercut_from_wor, 
                 R_valid, 
                 wcth_valid, 
                 p0=initial_guess,
@@ -111,7 +142,7 @@ class WOR_forecaster():
         # Если есть подобранные параметры, строим кривую
         if popt is not None:
             lam_fit, nw_fit, no_fit = popt
-            fw_fitted = self.watercut_calc(self.R, lam_fit, nw_fit, no_fit)
+            fw_fitted = self.watercut_from_wor(self.R, lam_fit, nw_fit, no_fit)
             plt.plot(self.R, fw_fitted, 'r-', label='Подгонка модели', linewidth=2)
         
         plt.xlabel('КИН (R)')
@@ -140,12 +171,15 @@ field = WOR_forecaster(**{'Swi': 0.3,
                               'Sor': 0.3,
                               'muo':2.47,
                               'muw':0.4,
-                              'krw':0.3,
+                              'krw':0.2,
                               'kro':1,
-                              'N': 18670*2,
+                              'N': 18680*2,
                               'lam': 1.01})
     # df
 field.set_hist_data(df)
 # %%
-popt, pcov = field.wct_fitting()
-field.plot_results(popt)
+df = field.wct_forecast()
+# %%
+n = field.limit_calc(field.nw_fit, field.no_fit, 0.98)
+
+# field.R_lim
